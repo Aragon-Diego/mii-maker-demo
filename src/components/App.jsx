d9abb0d54a6522d0b82e8f69e029f155d52b628a
aragon-diego <diego.aragoncursos@gmail.com>
2025-03-15 00:58:54 -0600

    dynamic head + some experiments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { Mii } from './Mii';
 
 function App() {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [0, 0, 20], fov: 25 }}>
       <Mii/>
     </Canvas>
   )
--- src/components/Mii.jsx
+++ src/components/Mii.jsx
@@ -1,13 +1,18 @@
 import { OrbitControls } from "@react-three/drei"
+import { MiiBodyM } from "./MiiBodyM"
+import { Head } from "./Head"
 
 export const Mii = () => {
   return (
     <>
       <OrbitControls/>
-      <mesh>
-        <boxGeometry/>
-        <meshNormalMaterial/>
-      </mesh>
+      <ambientLight intensity={1} color={'#FFF'}/>
+      <directionalLight
+        position={[-0.01, 1, 1]}
+        intensity={5}
+      />
+      <Head index={6}/>
+      <MiiBodyM/>
     </>
   )
 }

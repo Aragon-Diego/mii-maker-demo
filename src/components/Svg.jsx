01ff154697a9b94ac306065652d19cbcaf440ed5
aragon-diego <diego.aragoncursos@gmail.com>
2025-03-17 04:49:26 -0600

    svg errors

--- src/components/Svg.jsx
+++ src/components/Svg.jsx
@@ -4,7 +4,7 @@ import * as THREE from "three";
 import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 
 export const Svg = (props) => {
-  const svgPath =  "/assets/images/svg/" + (props.svg ? props.svg + '.svg' : 'nose-01.svg');
+  const svgPath =  "/mii-maker-demo/assets/images/svg/" + (props.svg ? props.svg + '.svg' : 'nose-01.svg');
   const svgData = useLoader(SVGLoader, svgPath);
   const shapes = svgData.paths.flatMap((path) => path.toShapes(false));
   const geometry = new THREE.ExtrudeGeometry(shapes, {
